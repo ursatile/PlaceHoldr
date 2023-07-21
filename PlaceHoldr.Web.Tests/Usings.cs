d89e400be0441feda99624321e22bd03003b1746
Dylan Beattie <dylan@dylanbeattie.net>
2023-07-21 15:43:26 +0100

    Commit before running R# code cleanup

--- PlaceHoldr.Web.Tests/Usings.cs
+++ PlaceHoldr.Web.Tests/Usings.cs
@@ -1,4 +1,2 @@
-global using Microsoft.Extensions.DependencyInjection;
-global using Xunit;
 global using Microsoft.AspNetCore.Mvc.Testing;
-global using Microsoft.AspNetCore.Mvc;
+global using Xunit;
