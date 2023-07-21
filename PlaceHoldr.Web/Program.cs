03344ff3a5a021646f7c68d6f23f6ae50982636f
Dylan Beattie <dylan@dylanbeattie.net>
2023-07-21 15:43:26 +0100

    Commit before running R# code cleanup

--- PlaceHoldr.Web/Program.cs
+++ PlaceHoldr.Web/Program.cs
@@ -1,11 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
 app.MapGet("/status", () => "OK");
-
 app.Run();
 
 // Required for our Program class to be visible from our test projects.
 // We could do this with InternalsVisibleTo, but that doesn't work with
 // NCrunch's test runner.
-public partial class Program {}
+public partial class Program { }
