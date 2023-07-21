07edda2bcb2d4a67e0996c353f2446643adcb765
Dylan Beattie <dylan@dylanbeattie.net>
2023-07-21 15:38:11 +0100

    Endpoint test works

--- PlaceHoldr.Web.Tests/Usings.cs
+++ PlaceHoldr.Web.Tests/Usings.cs
@@ -1,2 +1,4 @@
-global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.DependencyInjection;
 global using Xunit;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.AspNetCore.Mvc;
