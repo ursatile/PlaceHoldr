008fde31773051b971e01a8e516201e3b8ec9a93
Dylan Beattie <dylan@dylanbeattie.net>
2023-07-21 15:38:11 +0100

    Endpoint test works

--- PlaceHoldr.Web.Tests/UnitTest1.cs
+++ PlaceHoldr.Web.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
 namespace PlaceHoldr.Web.Tests;
 
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-
-    }
+public class WebTests : IClassFixture<WebApplicationFactory<Program>> {
+	[Fact]
+	public async Task Status_Endpoint_Returns_OK() {
+		var factory = new WebApplicationFactory<Program>();
+		var client = factory.CreateClient();
+		var response = await client.GetAsync("/status");
+		response.EnsureSuccessStatusCode();
+	}
 }
